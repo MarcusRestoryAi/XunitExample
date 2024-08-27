d8454fd60e66a5bbf72e5a9701c9508670a5f006
Marcus <marcus.henriksson@restory.ai>
2024-08-27 10:37:19 +0200

    Add project files.

---
+++ XunitExample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace XunitExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            int tal1 = 10, tal2 = 21;
+
+            //Init Math class
+            MyMath myMath = new MyMath();
+
+            Console.WriteLine($"Tal {tal1} är {(myMath.EvenOrOdd(tal1) ? "Jämnt" : "Ojämnt")}");
+            Console.WriteLine($"Tal {tal2} är {(myMath.EvenOrOdd(tal2) ? "Jämnt" : "Ojämnt")}");
+
+            Console.WriteLine($"Summan av {tal1} och {tal2} är {myMath.Add(tal1, tal2)}");
+        }
+
+        
+    }
+}
